af5899d295afa04468bd7feec04149b7e3aec0f7
Sou Hongchith <soulinthonehongchith@ou.edu>
2019-01-30 11:18:26 -0600

    Changes made

---
+++ HW1/HW1/Student.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Student //class name is a data type. treat it as int, double, etc.
+    {
+        //Instances:
+        public int SoonerID { get; set; } 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsOnProbation { get; set; }
+        public double GPA { get; set; }
+        private double BursarBalance; //Doesn't really matter if we have getter and setter here!
+
+        /// <summary>
+        /// Default constructor for the student class.
+        /// Sets all the instance variables to their default values.
+        /// </summary>
+        public Student()
+        {
+            SoonerID = 0;
+            FirstName = "";
+            LastName = "";
+            IsOnProbation = false;
+            GPA = 0.0;
+            BursarBalance = 0.0;
+        }
+
+        /// <summary>
+        /// This is a Constructor because the name of the method is the same as constructor's and no return type.
+        /// This is the overloaded constructor for student class that accepts SoonerID, FirstName, LastName,
+        /// BursarBalance.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="fName"></param>
+        /// <param name="lName"></param>
+        /// <param name="BursarBalance"></param>
+        public Student( int id, string fName, string lName, double BursarBalance)
+        {
+            SoonerID = id;
+            FirstName = fName;
+            LastName = lName;
+            this.BursarBalance = BursarBalance;
+        }
+
+        public void MakePayment (double amount)
+        {
+            if (amount > 0)
+            {
+                if (BursarBalance > 0)
+                {
+                    BursarBalance = BursarBalance - amount;
+                }
+            }
+        }
+
+        public double CheckBalance()
+        {
+            return BursarBalance;
+        }
+
+    }
+}
